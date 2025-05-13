7504170f8cbc7ba2a277579da9db761a99545ddd
Geraldo Benjamin Nainggolan <nainggolanben12@gmail.com>
2025-05-13 07:09:02 +0700

    feat add workspace collaborator repo

---
+++ internal/api/repository/workspace_collaborators_repository.go
@@ -0,0 +1,75 @@
+package repository
+
+import (
+	"frs-planning-backend/internal/entity"
+
+	"github.com/google/uuid"
+	"golang.org/x/net/context"
+	"gorm.io/gorm"
+)
+
+type (
+	WorkspaceCollaboratorRepository interface {
+		Add(ctx context.Context, tx *gorm.DB, collaborator entity.WorkspaceCollaborator) (entity.WorkspaceCollaborator, error)
+		Get(ctx context.Context, tx *gorm.DB, workspaceid uuid.UUID) ([]entity.WorkspaceCollaborator, error)
+		Delete(ctx context.Context, tx *gorm.DB, userid uuid.UUID, workspaceid uuid.UUID) (entity.WorkspaceCollaborator, error)
+	}
+
+	workspaceColllaboratorRepository struct {
+		db *gorm.DB
+	}
+)
+
+func NewWOrkspaceCollaboratorRepository(db *gorm.DB) WorkspaceCollaboratorRepository {
+	return &workspaceColllaboratorRepository{db}
+}
+
+func (r *workspaceColllaboratorRepository) Add(ctx context.Context, tx *gorm.DB, collaborator entity.WorkspaceCollaborator) (entity.WorkspaceCollaborator, error) {
+	if tx == nil {
+		tx = r.db
+	}
+
+	err := tx.WithContext(ctx).Create(&collaborator).Error
+	if err != nil {
+		return entity.WorkspaceCollaborator{}, err
+	}
+
+	return collaborator, nil
+}
+
+func (r *workspaceColllaboratorRepository) Get(ctx context.Context, tx *gorm.DB, workspaceID uuid.UUID) ([]entity.WorkspaceCollaborator, error) {
+	if tx == nil {
+		tx = r.db
+	}
+
+	var collaborators []entity.WorkspaceCollaborator
+	err := tx.WithContext(ctx).
+		Where("workspace_id = ?", workspaceID).
+		Find(&collaborators).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return collaborators, nil
+}
+
+func (r *workspaceColllaboratorRepository) Delete(ctx context.Context, tx *gorm.DB, userid uuid.UUID, workspaceid uuid.UUID) (entity.WorkspaceCollaborator, error) {
+	if tx == nil {
+		tx = r.db
+	}
+
+	var collaborator entity.WorkspaceCollaborator
+	err := tx.WithContext(ctx).
+		Where("user_id = ? AND workspace_id = ?", userid, workspaceid).
+		First(&collaborator).Error
+	if err != nil {
+		return entity.WorkspaceCollaborator{}, err
+	}
+
+	err = tx.WithContext(ctx).Delete(&collaborator).Error
+	if err != nil {
+		return entity.WorkspaceCollaborator{}, err
+	}
+
+	return collaborator, nil
+}
