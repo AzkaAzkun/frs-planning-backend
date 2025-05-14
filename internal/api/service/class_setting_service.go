050b28e4d485f3bae9b69f747a67dad7cea9c5bf
Geraldo Benjamin Nainggolan <nainggolanben12@gmail.com>
2025-05-14 19:01:21 +0700

    feat add class setting service

---
+++ internal/api/service/class_setting_service.go
@@ -0,0 +1,63 @@
+package service
+
+import (
+	"frs-planning-backend/internal/api/repository"
+	"frs-planning-backend/internal/dto"
+	"frs-planning-backend/internal/entity"
+
+	"github.com/google/uuid"
+	"golang.org/x/net/context"
+	"gorm.io/gorm"
+)
+
+type (
+	ClassSettingService interface {
+		Create(ctx context.Context, req dto.CreateClassSettingRequest, userid string) (dto.ClassSettingResponse, error)
+		Clone(ctx context.Context, userid string, req dto.CloneClassSettingRequest) (dto.ClassSettingResponse, error)
+	}
+
+	classSettingService struct {
+		classSettingRepository repository.ClassSettingRepository
+		db                     *gorm.DB
+	}
+)
+
+func NewClassSettingService(classSettingRepository repository.ClassSettingRepository, db *gorm.DB) ClassSettingService {
+	return &classSettingService{
+		classSettingRepository: classSettingRepository,
+		db:                     db,
+	}
+}
+
+func (s *classSettingService) Create(ctx context.Context, req dto.CreateClassSettingRequest, userid string) (dto.ClassSettingResponse, error) {
+	classSetting, err := s.classSettingRepository.Create(ctx, nil, entity.ClassSettings{
+		Name:       req.Name,
+		Permission: req.Permission,
+		UserID:     uuid.MustParse(userid),
+		Status:     "CLONE",
+	})
+	if err != nil {
+		return dto.ClassSettingResponse{}, nil
+	}
+	return dto.ClassSettingResponse{
+		ID:         classSetting.ID.String(),
+		Name:       classSetting.Name,
+		User_id:    classSetting.UserID.String(),
+		Permission: classSetting.Permission,
+		Status:     classSetting.Status,
+	}, nil
+}
+
+func (s *classSettingService) Clone(ctx context.Context, userid string, req dto.CloneClassSettingRequest) (dto.ClassSettingResponse, error) {
+	cloneClassSetting, err := s.classSettingRepository.Clone(ctx, nil, uuid.MustParse(userid), uuid.MustParse(req.ClassSettingId))
+	if err != nil {
+		return dto.ClassSettingResponse{}, err
+	}
+	return dto.ClassSettingResponse{
+		ID:         cloneClassSetting.ID.String(),
+		Name:       cloneClassSetting.Name,
+		User_id:    cloneClassSetting.UserID.String(),
+		Permission: cloneClassSetting.Permission,
+		Status:     cloneClassSetting.Status,
+	}, nil
+}
